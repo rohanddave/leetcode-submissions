9e81e953c9ffb5a2c9c762abf47d37104119d05f
Rohan Dave <rohandewangdave@gmail.com>
2026-07-15 20:56:41 -0700

    Time: 2934 ms (9.65%) | Memory: 71.2 MB (5.71%) - LeetSync

--- 327-count-of-range-sum/count-of-range-sum.py
+++ 327-count-of-range-sum/count-of-range-sum.py
@@ -1,60 +1,71 @@
-class FenwickTree:
-    def __init__(self, n):
-        self.n = n
-        self.tree = [0] * (n + 1)
-
-    def update(self, i, delta):
-        while i <= self.n:
-            self.tree[i] += delta
-            i += i & -i
-
-    def query(self, i):
-        ans = 0
-        while i > 0:
-            ans += self.tree[i]
-            i -= i & -i
-        return ans
-
-    def range_query(self, l, r):
-        return self.query(r) - self.query(l - 1)
-
+class TreeNode:
+    def __init__(self, l, r, val=0, left=None, right=None):
+        self.l = l 
+        self.r = r
+        self.val = val 
+        self.left = left 
+        self.right = right 
+    
+class SegmentTree:
+    def __init__(self, n): 
+        self.root = self._build(0, n - 1)
+    
+    def _build(self, l, r): 
+        node = TreeNode(l, r)
+        if l == r:
+            return node 
+        
+        m = (l + r) // 2
+        node.left = self._build(l, m)
+        node.right = self._build(m + 1, r)
+        node.val = node.left.val + node.right.val
+        return node
+    
+    def query(self, node, ql, qr): 
+        if ql <= node.l and qr >= node.r:
+            return node.val 
+        if ql > node.r or qr < node.l:
+            return 0
+    
+        left = self.query(node.left, ql, qr)
+        right = self.query(node.right, ql, qr)
+        return left + right
+    
+    def update(self, node, idx, delta): 
+        if node.l == node.r: 
+            node.val += delta 
+            return 
+        
+        m = (node.l + node.r) // 2
+        if idx <= m:
+            self.update(node.left, idx, delta) 
+        else:
+            self.update(node.right, idx, delta)
+        
+        node.val = node.left.val + node.right.val
+        return
+    
 class Solution:
     def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
-        '''
-        goal: return number of range sums lie in [lower, upper]
-
-        range sum S(i, j) = sum of elements between indices i, j 
-        '''
-        n = len(nums)
         prefix = [0]
-
-        for i, num in enumerate(nums):
-            prefix.append(prefix[-1] + num)
-
-        # co-ordinate compression for efficient fenwick tree
-        values = sorted(set(prefix))
-        index = {
-            val: i + 1
-            for i, val in enumerate(values)
+        for num in nums:
+            prefix.append(num + prefix[-1])
+        
+        sorted_prefix = sorted(set(prefix))
+        rank = { 
+            val: idx
+            for idx, val in enumerate(sorted_prefix)
         }
-
-        # create fenwick tree with compressed co-ordinates 
-        # NOTE: fenwick tree stores the frequency of prefix sum at compressed co-ordinate index; tree[i] = val means the compressed co-ordinate index is i and the val is the frequency of the prefix sum
-        seen = FenwickTree(len(values))
-        seen.update(index[0], 1)
+        seg_tree = SegmentTree(len(rank) + 1)
 
         count = 0
-        for i in range(1, len(prefix)):
-            left_val, right_val = prefix[i] - upper, prefix[i] - lower
+        for p in prefix: 
+            left_boundry = p - upper 
+            right_boundry = p - lower
 
-            # we need binary search because left_val and right_val might not exist exactly in fenwick tree, binary search gives us the position of arbitary values 
-            # NOTE: fenwick tree is 1 indexed so binary search bounds needs to be handled accrodingly since it return 0 based index
-            left_idx = bisect_left(values, left_val) # first greater than equal to 
-            right_idx = bisect_right(values, right_val) - 1 # bisect_right = first greater than x; bisect_right - 1 = last less than equal to
-
-            count += seen.range_query(left_idx + 1, right_idx + 1) # converting 0 based index to 1 based index
-
-            seen.update(index[prefix[i]], 1) # increment the freq of prefix[i] by 1
-
-
+            ql, qr = bisect_left(sorted_prefix, left_boundry), bisect_right(sorted_prefix, right_boundry) - 1
+            count += seg_tree.query(seg_tree.root, ql, qr)
+            seg_tree.update(seg_tree.root, rank[p], 1)
+        
         return count
+        
