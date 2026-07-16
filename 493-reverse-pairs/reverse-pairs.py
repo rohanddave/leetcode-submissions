c0d9ba8993c91a521800a09a90ee590f316ea7bc
Rohan Dave <rohandewangdave@gmail.com>
2026-07-15 19:54:50 -0700

    Time: 1266 ms (5.50%) | Memory: 44.5 MB (5.19%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.py
+++ 493-reverse-pairs/reverse-pairs.py
@@ -1,60 +1,64 @@
-class FenwickTree: 
-    def __init__(self, n):
-        self.tree = [0] * (n + 1)
-    
-    def query(self, i): 
-        ans = 0 
-        while i > 0: 
-            ans += self.tree[i] 
-            i -= i & -i
-        return ans
-    
-    def update(self, i, delta): 
-        while i < len(self.tree):
-            self.tree[i] += delta 
-            i += i & -i
+class TreeNode: 
+    def __init__(self, l, r, val=0, left=None, right=None):
+        self.l = l 
+        self.r = r
+        self.val = val
+        self.left = left
+        self.right = right
 
+class SegmentTree:
+    def __init__(self, n):
+        self.root = self._build(1, n)
+    
+    def _build(self, l, r): 
+        node = TreeNode(l, r)
+        if l == r:
+            node.val = 0
+            return node
+        
+        m = (l + r) // 2
+        node.left = self._build(l, m)
+        node.right = self._build(m + 1, r)
+        node.val = node.left.val + node.right.val 
+        return node
+    
+    def query(self, node, ql, qr):
+        if ql <= node.l and qr >= node.r: 
+            return node.val 
+        if node.l > qr or node.r < ql:
+            return 0 
+        
+        left = self.query(node.left, ql, qr)
+        right = self.query(node.right, ql, qr)
+        return left + right        
+    
+    def update(self, node, i, delta): 
+        if node.l == node.r:
+            node.val += delta 
+            return 
+        
+        mid = (node.l + node.r) // 2
+        if i <= mid: 
+            self.update(node.left, i, delta)
+        else: 
+            self.update(node.right, i, delta)
+        
+        node.val = node.left.val + node.right.val
+    
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
-        '''
-        problem: 
-        - reverse pair (i, j)
-            - 0 <= i < j 
-            - nums[i] > 2 * nums[j]
-        
-        goal: return number of reverse pairs in array
-
-        observations:
-        - for nums[j] we need to pair with i's (before j) that are greater than 2 * nums[j]
-        - so we need to know all the numbers before nums[j] that are greater than 2 * nums[j]
-        - bottleneck is rescanning from 0 - j to find ith element 
-
-        approach: 
-        - scan left to right 
-        - fenwick tree answers prefix sum i.e. how many values <= target
-        - query for (2 * nums[j]) + 1 from fenwick tree (log n)
-        - insert each element into fenwick tree (maybe coordinate compression)
-
-        brute force: 
-        for j in range(n):
-            for i in range(j):
-                if nums[i] > 2 * nums[j]:
-                    count += 1
-        
-
-        '''
-        values = sorted(set(nums))
+        sorted_nums = sorted(set(nums))
         rank = {
-            val: i + 1
-            for i, val in enumerate(values)
+            val: i + 1 for i, val in enumerate(sorted_nums)
         }
-        fenwick = FenwickTree(len(nums))
-        count = 0 
-        for j, num in enumerate(nums): 
-            idx = bisect_right(values, 2 * nums[j])
-            count_lte = fenwick.query(idx)
-            count += j - count_lte
-            fenwick.update(rank[nums[j]], 1)
-        return count
 
+        seg_tree = SegmentTree(len(rank))
+        count = 0 
+        for j in range(len(nums)):
+            idx = rank[nums[j]]
+            ql, qr = bisect_right(sorted_nums, 2 * nums[j]), float('inf')
+            count += seg_tree.query(seg_tree.root, ql + 1, qr)
+            seg_tree.update(seg_tree.root, idx, 1)
+        
+        return count
         
