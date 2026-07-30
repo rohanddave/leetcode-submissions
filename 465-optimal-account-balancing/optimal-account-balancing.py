28bde9bae73d999b996acea027740ea501a0d2ea
Rohan Dave <rohandewangdave@gmail.com>
2026-07-30 00:15:08 -0700

    Time: 282 ms (15.41%) | Memory: 19.3 MB (44.30%) - LeetSync

---
+++ 465-optimal-account-balancing/optimal-account-balancing.py
@@ -0,0 +1,64 @@
+class Solution:
+    def minTransfers(self, transactions: List[List[int]]) -> int:
+        '''
+        problem: 
+        - transactions[i] = [from, to, amount] 
+        
+        goal: return min number of transactions to settle debt 
+
+        example: 
+        Input: transactions = [[0,1,10],[2,0,5]]
+        Output: 2
+
+        0 -> 1 = 10 
+        2 -> 0 = 5
+
+        0: -5 
+        1: +10
+        2: -5
+        ------
+
+        0: -4
+        1: 4
+        2: 0
+
+        observations: 
+        - we need each person's balance to be 0
+        - we have:
+            - people that need to receive money (-ve)
+            - people that need to send money (+ve )
+            - neither need to receive or send money (0)
+        
+        approach:
+        for each person that needs to send money:
+            try to send money to every person person that needs to receive amount <= surplus current person has 
+        
+        state: 
+        - 
+        '''
+        balances = [0] * 12
+        for from_id, to_id, amt in transactions:
+            balances[from_id] -= amt # -ve balance means this person is owed money
+            balances[to_id] += amt # +ve balance means this person needs to pay money
+
+        # i index is for each positve balance which should be paired with a negative j index
+
+        # +5, -10
+        def dfs(i):
+            while i < len(balances) and balances[i] == 0:
+                i += 1
+            
+            if i == len(balances): 
+                return 0
+
+            count = float('inf')
+            for j in range(i + 1, len(balances)):
+                # if opposite signs and neither is 0
+                balance_i_cp, balance_j_cp = balances[i], balances[j]
+                if balances[j] * balances[i] < 0:
+                    balances[j] += balances[i] 
+                    count = min(count, 1 + dfs(i + 1))
+                    balances[j] -= balances[i]
+            return count
+
+        return dfs(0)
