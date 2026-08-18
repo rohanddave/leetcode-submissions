b53748c067edc99ecec65dc01f87978dcb77e09e
Rohan Dave <rohandewangdave@gmail.com>
2026-08-17 22:38:46 -0700

    Time: 0 ms (100.00%) | Memory: 19.6 MB (20.05%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -10,7 +10,5 @@ class Solution:
             half = helper(x, n // 2)
             return half * half * (x if n % 2 != 0 else 1)
         
-        res = helper(x, abs(n) // 2)
-        res = res * res 
-        res = res * x if n % 2 != 0 else res
+        res = helper(x, abs(n))
         return 1/res if n < 0 else res
