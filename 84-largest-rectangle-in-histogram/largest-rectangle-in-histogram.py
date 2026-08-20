bcbd7b6b4bc4cc67f493972aa6d8be055de8c698
Rohan Dave <rohandewangdave@gmail.com>
2026-08-20 00:11:57 -0700

    Time: 215 ms (9.08%) | Memory: 31.5 MB (41.44%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -2,11 +2,21 @@ class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         n = len(heights)
 
-        stack = [] 
-        prev_smaller = [-1] * n 
-
-        for i in range(n): 
+        next_smaller = [n] * n
+        stack = []
+        for i in range(n - 1, -1, -1): 
             while stack and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+            
+            if stack: 
+                next_smaller[i] = stack[-1]
+
+            stack.append(i)
+        
+        stack = [] 
+        prev_smaller = [-1] * n
+        for i in range(n): 
+            while stack and heights[stack[-1]] >= heights[i]: 
                 stack.pop()
             
             if stack: 
@@ -14,25 +24,12 @@ class Solution:
             
             stack.append(i)
         
-        stack = [] 
-        next_smaller = [n] * n 
-        
-        for i in range(n - 1, -1, -1): 
-            while stack and heights[stack[-1]] >= heights[i]: 
-                stack.pop()
-            
-            if stack: 
-                next_smaller[i] = stack[-1]
-            
-            stack.append(i)
-        
-        ans = 0
+        res = float('-inf')
+        for i, height in enumerate(heights): 
+            l_boundary = prev_smaller[i] + 1
+            r_boundary = next_smaller[i] - 1
 
-        for i in range(n): 
-            left_wall = prev_smaller[i] + 1
-            right_wall = next_smaller[i] - 1
-            width = right_wall - left_wall + 1
-            area = heights[i] * width 
-            ans = max(ans, area)
-        
-        return ans
+            width = r_boundary - l_boundary + 1
+            res = max(res, height * width)
+            
+        return res
