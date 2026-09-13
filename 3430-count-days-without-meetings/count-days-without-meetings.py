508def348703bcd1764e797cbf0aa2fa30a66fc3
Rohan Dave <rohandewangdave@gmail.com>
2026-09-13 10:32:49 -0700

    Time: 235 ms (5.79%) | Memory: 55.5 MB (25.00%) - LeetSync

---
+++ 3430-count-days-without-meetings/count-days-without-meetings.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        def does_overlap(a, b): 
+            return not (a[0] > b[1] or a[1] < b[0])
+        
+        meetings.sort()
+        merged = [meetings[0]] 
+        for i in range(1, len(meetings)): 
+            if does_overlap(merged[-1], meetings[i]):
+                merged[-1][1] = max(meetings[i][1], merged[-1][1])
+            else: 
+                merged.append(meetings[i])
+        
+        available = [1, days]
+        res = days
+        for interval in merged:
+            intersection = [max(available[0], interval[0]), min(available[1], interval[1])] 
+            overlap = intersection[1] - intersection[0] + 1
+            if overlap < 0:
+                continue
+            res -= overlap
+        return res
+
+
+        
+        
+
+
+        
