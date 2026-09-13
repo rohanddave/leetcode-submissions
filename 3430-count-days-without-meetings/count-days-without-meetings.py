11edfa431ae7ed76577b12e61f8afc6caaf6b540
Rohan Dave <rohandewangdave@gmail.com>
2026-09-13 10:43:30 -0700

    Time: 215 ms (9.03%) | Memory: 55.6 MB (12.50%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.py
+++ 3430-count-days-without-meetings/count-days-without-meetings.py
@@ -5,25 +5,23 @@ class Solution:
         
         meetings.sort()
         merged = [meetings[0]] 
+        available = [1, days]
+        res = days
+
         for i in range(1, len(meetings)): 
             if does_overlap(merged[-1], meetings[i]):
                 merged[-1][1] = max(meetings[i][1], merged[-1][1])
             else: 
+                interval = merged[-1]
+                intersection = [max(available[0], interval[0]), min(available[1], interval[1])]
+                overlap = intersection[1] - intersection[0] + 1
+                if overlap > 0:
+                    res -= overlap
                 merged.append(meetings[i])
-        
-        available = [1, days]
-        res = days
-        for interval in merged:
-            intersection = [max(available[0], interval[0]), min(available[1], interval[1])] 
-            overlap = intersection[1] - intersection[0] + 1
-            if overlap < 0:
-                continue
+        interval = merged[-1]
+        intersection = [max(available[0], interval[0]), min(available[1], interval[1])]
+        overlap = intersection[1] - intersection[0] + 1
+        if overlap > 0:
             res -= overlap
         return res
 
-
-        
-        
-
-
-        
