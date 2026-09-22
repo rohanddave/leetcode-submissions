b893d0f728d823550e058b4a8cdbb17306c4288e
Rohan Dave <rohandewangdave@gmail.com>
2026-09-22 07:58:09 -0700

    Time: 0 ms (100.00%) | Memory: 19.7 MB (6.94%) - LeetSync

---
+++ 364-nested-list-weight-sum-ii/nested-list-weight-sum-ii.py
@@ -0,0 +1,66 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        The result is undefined if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        The result is undefined if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSumInverse(self, nestedList: list[NestedInteger]) -> int:
+        max_depth = float('-inf')
+        depths = []
+
+        def dfs(arr, depth): 
+            nonlocal max_depth, depths
+            
+            for j in range(len(arr)):
+                if arr[j].isInteger():
+                    max_depth = max(max_depth, depth)
+                    depths.append((arr[j].getInteger(), depth))
+                else:
+                    dfs(arr[j].getList(), depth + 1)
+        dfs(nestedList, 1)
+        res = 0 
+        for num, depth in depths:
+            weight = max_depth - depth + 1 
+            res += num * weight
+        return res
+
+
+        
