4493d18bac5470df4eb430bd1b5045ba9721afd6
Rohan Dave <rohandewangdave@gmail.com>
2026-09-23 08:03:15 -0700

    Time: 1 ms (60.94%) | Memory: 21 MB (60.00%) - LeetSync

--- 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.py
+++ 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.py
@@ -6,18 +6,53 @@
 #         self.right = right
 class Solution:
     def closestKValues(self, root: TreeNode | None, target: float, k: int) -> list[int]:
-        heap = [] 
+        # heap = [] 
 
-        def dfs(node): 
+        # def dfs(node): 
+        #     if not node:
+        #         return 
+            
+        #     heapq.heappush(heap, (-1 * abs(target - node.val), node.val))
+        #     if len(heap) > k:
+        #         heapq.heappop(heap)
+        #     dfs(node.left)
+        #     dfs(node.right)
+        # dfs(root)
+        # return [val for _, val in heap]
+
+        inorder = [] 
+
+        def dfs(node):
             if not node:
                 return 
             
-            heapq.heappush(heap, (-1 * abs(target - node.val), node.val))
-            if len(heap) > k:
-                heapq.heappop(heap)
-            dfs(node.left)
+            dfs(node.left) 
+            inorder.append(node.val)
             dfs(node.right)
+        
         dfs(root)
-        return [val for _, val in heap]
+
+        l, r = 0, len(inorder)
+        while l < r:
+            m = (l + r) // 2
+            if inorder[m] >= target:
+                r = m
+            else:
+                l = m + 1
+        
+        i, j = l - 1, l
+        res = []
+        for _ in range(k):
+            diff_i = abs(target - inorder[i]) if i >= 0 else float('inf')
+            diff_j = abs(target - inorder[j]) if j < len(inorder) else float('inf')
+            if diff_i < diff_j:
+                res.append(inorder[i])
+                i -= 1
+            else: 
+                res.append(inorder[j])
+                j += 1
+        return res
+            
+
         
         
